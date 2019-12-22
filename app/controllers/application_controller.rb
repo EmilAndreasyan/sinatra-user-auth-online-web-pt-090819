da040eb6e380075288e284faf546a9604332ec77
Emil Andreasyan <emil.andreasyan@yahoo.com>
2019-12-22 09:42:09 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < Sinatra::Base
     enable :sessions
     set :session_secret, "secret"
   end
-
+  
   get '/' do
     erb :home
   end
@@ -20,10 +20,11 @@ class ApplicationController < Sinatra::Base
     @user = User.new(name: params["name"], email: params["email"], password: params["password"])
     @user.save
     session[:user_id] = @user.id
-
+    puts params
     redirect '/users/home'
   end
-
+  
+  
   get '/sessions/login' do
 
     # the line of code below render the view page in app/views/sessions/login.erb
